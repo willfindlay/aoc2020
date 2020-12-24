ec5f3508e0a43165cf51820a82d212aff4949e2c
William Findlay <william@williamfindlay.com>
2020-12-24 10:54:01 -0500

    Add some documentation to day 12

--- will/day12/src/main.rs
+++ will/day12/src/main.rs
@@ -14,30 +14,41 @@ fn main() -> Result<()> {
     Ok(())
 }
 
+/// Solve part 1
 fn part1(input: &Vec<(char, i32)>) -> Result<()> {
+    // Unfortunately, part 2 required a full reimplementation, so I had to move
+    // the original Ferry implementation into a module
     let mut ferry = part1::Ferry::new();
 
+    // Dispatch the operations
     for (operation, units) in input {
         ferry.do_operation(*operation, *units);
     }
 
+    // Find the manhattan distance
     println!("{}", ferry.get_manhattan());
 
     Ok(())
 }
 
+/// Solve part 2
 fn part2(input: &Vec<(char, i32)>) -> Result<()> {
+    // Unfortunately, part 2 required a full reimplementation, so I had to
+    // re-write the original Ferry implementation in a new module
     let mut ferry = part2::Ferry::new();
 
+    // Dispatch the operations
     for (operation, units) in input {
         ferry.do_operation(*operation, *units);
     }
 
+    // Find the manhattan distance
     println!("{}", ferry.get_manhattan());
 
     Ok(())
 }
 
+/// Parse the input into a list of (operation, value) tuples
 fn parse_input(input: &String) -> Result<Vec<(char, i32)>> {
     let line_re = Regex::new(r"([NSEWLRF])(\d+)")?;
     let mut parsed = vec![];
@@ -53,6 +64,7 @@ fn parse_input(input: &String) -> Result<Vec<(char, i32)>> {
     Ok(parsed)
 }
 
+/// An unfortunate module for part 1's implementation
 mod part1 {
     pub struct Ferry {
         direction: char,
@@ -69,10 +81,12 @@ mod part1 {
             }
         }
 
+        /// Get the manhattan distance from the origin
         pub fn get_manhattan(&self) -> i32 {
             self.x.abs() + self.y.abs()
         }
 
+        /// Dispatch an @operation with @value
         pub fn do_operation(&mut self, operation: char, value: i32) {
             match operation {
                 'N' => self.travel_direction('N', value),
@@ -86,6 +100,7 @@ mod part1 {
             }
         }
 
+        /// Turn the ship left @value degrees
         fn turn_left(&mut self, value: i32) {
             if value <= 0 {
                 return;
@@ -100,6 +115,7 @@ mod part1 {
             return self.turn_left(value - 90);
         }
 
+        /// Turn the ship right @value degrees
         fn turn_right(&mut self, value: i32) {
             if value <= 0 {
                 return;
@@ -114,10 +130,12 @@ mod part1 {
             return self.turn_right(value - 90);
         }
 
+        /// Head in the current direction by @value units
         fn go_straight(&mut self, value: i32) {
             self.travel_direction(self.direction, value);
         }
 
+        /// Head in @direction by @value units
         fn travel_direction(&mut self, direction: char, value: i32) {
             match direction {
                 'N' => self.y += value,
@@ -130,12 +148,15 @@ mod part1 {
     }
 }
 
+/// An unfortunate module for part 2's implementation
 mod part2 {
+    /// The waypoint, relative to the ship. Let's not do anything super fancy here
     struct Waypoint {
         pub x: i32,
         pub y: i32,
     }
 
+    /// The ferry
     pub struct Ferry {
         waypoint: Waypoint,
         x: i32,
@@ -143,6 +164,7 @@ mod part2 {
     }
 
     impl Ferry {
+        /// Create a new Ferry. Starts at 0,0 with a waypoint at 10,1
         pub fn new() -> Self {
             Ferry {
                 waypoint: Waypoint { x: 10, y: 1 },
@@ -151,10 +173,12 @@ mod part2 {
             }
         }
 
+        /// Get the manhattan distance from the origin
         pub fn get_manhattan(&self) -> i32 {
             self.x.abs() + self.y.abs()
         }
 
+        /// Dispatch an @operation with a @value
         pub fn do_operation(&mut self, operation: char, value: i32) {
             match operation {
                 'N' => self.move_waypoint('N', value),
@@ -168,6 +192,7 @@ mod part2 {
             }
         }
 
+        /// Rotate the waypoint left @value degrees relative to the ship
         fn rotate_left(&mut self, value: i32) {
             if value <= 0 {
                 return;
@@ -179,6 +204,7 @@ mod part2 {
             return self.rotate_left(value - 90);
         }
 
+        /// Rotate the waypoint right @value degrees relative to the ship
         fn rotate_right(&mut self, value: i32) {
             if value <= 0 {
                 return;
@@ -190,11 +216,13 @@ mod part2 {
             return self.rotate_right(value - 90);
         }
 
+        /// Travel toward the waypoint @value times
         fn travel(&mut self, value: i32) {
             self.x += self.waypoint.x * value;
             self.y += self.waypoint.y * value;
         }
 
+        /// Move the waypoint in a cardinal @direction @value times
         fn move_waypoint(&mut self, direction: char, value: i32) {
             match direction {
                 'N' => self.waypoint.y += value,
